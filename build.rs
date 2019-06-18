b28438c14fcacb336c3c61fdef981beea5b3a49c
pcPowerJG <43109784+pcPowerJG@users.noreply.github.com>
2019-06-18 13:49:54 +0300

    Add files via upload

--- build.rs
+++ build.rs
@@ -4,4 +4,10 @@ fn main() {
     cc::Build::new()
         .file("/home/pokemon/ThRustGorge/src/c/math.c")
         .compile("libmath.a");
+    cc::Build::new()
+        .file("/home/pokemon/ThRustGorge/src/c/extern.c")
+        .flag("-fPIC")
+        .flag("-ldl")
+        .flag("-rdynamic")
+        .compile("libto_extern.a");
 }

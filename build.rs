06f4b2d14e72a66c52d99522c2d810ecdf517ea5
pcPowerJG <43109784+pcPowerJG@users.noreply.github.com>
2019-06-19 07:53:56 +0400

    Update build.rs

--- build.rs
+++ build.rs
@@ -2,10 +2,10 @@ extern crate cc;
 
 fn main() {
     cc::Build::new()
-        .file("/home/pokemon/ThRustGorge/src/c/math.c")
+        .file("./src/c/math.c")
         .compile("libmath.a");
     cc::Build::new()
-        .file("/home/pokemon/ThRustGorge/src/c/extern.c")
+        .file("./src/c/extern.c")
         .flag("-fPIC")
         .flag("-ldl")
         .flag("-rdynamic")

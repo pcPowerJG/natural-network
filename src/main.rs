c3e7dc7949ca7be7892495d550adc1fddf92908f
pcPowerJG <43109784+pcPowerJG@users.noreply.github.com>
2019-06-30 18:22:58 +0300

    поправил чтение файлов

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,13 @@
 extern crate ThRustGorge;
 use ThRustGorge::Language;
 
+extern crate libc;
+use libc::*;
+
+use std::ffi::CString;
+use std::ptr;
+use std::ffi::CStr;
+
 use std::net::*;
 use std::thread;
 use std::io::Write;
@@ -10,6 +17,14 @@ use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
+use std::io;
+
+//#[link(name = "math")]
+#[link(name="open_file")]
+extern {
+	//char* read_file(char* path)
+	fn read_file(path: *const libc::c_char) -> *const libc::c_char;	
+}
 
 fn main() {
     //println!("Hello, world!");
@@ -22,217 +37,20 @@ fn main() {
         let end_f: Vec<&str> = arg1.as_str().split('.').collect();
         if end_f.len() > 1 && end_f[1].clone() == "gorge" {
             // path: (argument as String)
-            let mut path: String = end_f[0].clone().to_string();
+            let mut path: String = end_f[0].to_string();
             path.push('.');
             path += end_f[1].clone();
-            //println!("path to file: {}\n\n", path);
-            let file = File::open(path.as_str()).unwrap();
-            let mut buf_reader = BufReader::new(file);
-            let mut contents = String::new();
-            buf_reader.read_to_string(&mut contents).unwrap();
-            //assert_eq!(contents, "Hello, world!");
-            o.get_(contents, "".to_string(), 0);  
-            //o.drop(); 
-            //drop(contents);
-            //drop(buf_reader);
-            //panic!("");
-        }       
-        //o.get_("".to_string(), "".to_string(), 0);
-    }
-    /*let listener = TcpListener::bind("127.0.0.1:9273").unwrap();
-    println!("listening started, ready to accept");
-    //fn read_to_end(&mut self, buf: &mut Vec<u8>) -> Result<usize>
-    for stream in listener.incoming() {
-        thread::spawn(move| | {
-            let mut stream = match stream {
-                Ok(A) => { A },
-                Err(e) => { 
-                    println!("ошибка получения пользователя."); 
-                    println!("{:?}", e); 
-                    panic!("ошибка получения потока пользователя"); 
-                    stream.unwrap()
-                },
-            };
-            let mut bytes: Vec<u8> = Vec::new();
-            match stream.read_to_end(&mut bytes) {
-                Ok(A) => {  },
-                Err(e)=> { println!("не удалось прочитать сообщение"); },
-            };
-            let row_: String = match String::from_utf8(bytes) {
-                Ok(A) => { A },
-                Err(e)=> { println!("ошибка преобразования строки"); "".to_string() },
-            };
-            if row_ != "".to_string() {
-
+            {                
+                let mut contents = String::new();
+		unsafe {
+			let result = match CStr::from_ptr(read_file(CString::new(path).expect("ошибка передачи аргумента. ошибка преобразования.").as_ptr())).to_str() {
+				Ok(A) => { A },
+				Err(e)=> { panic!("не удалось открыть файл, ошибка преобразования строк. файл должен быть в кодировке utf-8"); "" },
+			};		
+			contents = result.to_string();
+		}
+                o.get_(contents, "".to_string(), 0);  
             }
-        });
-    }*/
-    //self.get_all_func(text.clone(), temp_to_func.clone());
-	/*o.get_("
-
-object true_ =true
-object false_ =false
-object other =12
-object return_variable
-
-struct str1
-    object hello
-    object hello1
-end
-func test_array(void)
-    array ar =one,two,tree
-    remove ar[2]
-    remove ar[1]
-    remove ar[0]
-    remove ar
-end_func
-
-func for_test(void)
-    print OTHER:
-    push(other, \\n)
-    print other
-
-    print RETURN_VARIABLE:
-    print return_variable
-
-    print expand variable
-    print return_variable
-    print to:
-    expand(return_variable, return_variable, return_variable)
-    print return_variable    
-end_func
-
-loop 
-    if other > 60
-        print buy cirkle
-        print -----------------------------
-        break
-    end
-    other = other + 0.5 : math    
-end_loop
-import(mylib.out, @lib)
-    extern_func(lib, say_hello_world)
-    extern_func(lib, say_and_return, other, return_variable)
-    extern_func(lib, say, other)
-    extern_func(lib, say_hello_world)
-close_import(lib)
-print -----------------------------
-    for_test(other)
-print -----------------------------
-
-
-exit(0)
-".to_string(), "".to_string(), 0); *//*
-object toto_other=21
-object gb =Good Bue looper_
-func hello(arg1)
-
-    if other < to_other
-        hello_(other)        
-    end
-    print arg1
-end_func
-
-func looper_(arg1)
-    loop
-        if other < toto_other
-            break
-        end
-        other = other + 2 : math
-    end_loop
-end_func
-
-func hello_(arg1)
-    other = other + 1 : math
-    if other < to_other
-        hello_(other)        
-    end
-end_func
-
-hello(other)
-
-println!("\n\nall: \n\n{}", o.get_all_func("
-object other=0
-object to_other=10
-object toto_other=21
-object gb =Good Bue looper_
-func hello(arg1)
-    print arg1
-end_func
-func looper_(arg1)
-    loop
-        if other > to_other
-            break
-        end
-        other = other + 1 : math
-        end_loop
-    hello(other)
-    print gb
-end_func
-
-looper_(other)
-".to_string(), "hello,looper_".to_string()));*/
-	/*o.get_("
-object other=1
-object to_other=10
-object toto_other=5
-object gb =Good Bue looper_
-
-func plus_one(arg2)
-    loop
-        if other > toto_other 
-            break
-        end
-        other = other + 2 : math
-    end_loop
-end_func
-
-func hello(arg1)
-    print arg1
-end_func
-func looper_(arg1)
-    loop#plus_one
-        if other > to_other
-            break
-        end
-        plus_one(other)        
-        other = other + 1 : math
-    end_loop
-
-    hello(other)
-    print gb
-
-end_func
-
-looper_(other)
-
-".to_string(), "".to_string(), false, 0);*/
-	//o.unsafe_print_bufs_and_vals();	
-    //dbg!(o.get_value("obj2".to_string()));
-
-    /*
-        \n server serv0
-        \n server serv1 =127.0.0.1:8080
-        \n obj1 = serv1 
-        \n obj1 = obj2
-        \n object obj3 
-        \n obj3 = ney1 
-        \n create ney0 [5] 
-        \n ney1 = ney0
-        \n print ney1
-        \n ney02 = obj3
-        \n ney1 = { 1.2 1.4 1.9 2.0 -5.6 }
-        \n remove obj3
-        \n print hello
-        \n hello = ney1
-        \n print hello
-        \n hello =Hello, World!== '\" lasoqw U!@` 2?
-        \n print hello 
-        \n print obj2
-        \n print ney1
-        \n print ney02
-        \n print ney0
-        \n print serv1
-        \n print obj4    
-    */
+        }
+    }   
 }
